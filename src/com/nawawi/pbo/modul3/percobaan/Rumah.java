879eec2e27cf9d977f933b098b01ec6262b73ff2
AhmadMCer <ahmadmcer@gmail.com>
2021-10-21 14:34:16 +0700

    Modul3: Add percobaan files.

---
+++ src/com/nawawi/pbo/modul3/percobaan/Rumah.java
@@ -0,0 +1,9 @@
+package com.nawawi.pbo.modul3.percobaan;
+
+public class Rumah {
+    String alamatRumah;
+
+    public void setAlamat(String x) {
+        alamatRumah = x;
+    }
+}
---
+++ src/com/nawawi/pbo/modul3/percobaan/Tes.java
@@ -0,0 +1,9 @@
+package com.nawawi.pbo.modul3.percobaan;
+
+public class Tes {
+    public static void main(String[] args) {
+        Rumah r = new Rumah();
+        r.setAlamat("Alamat saya di Jalan Pramuka");
+        System.out.println(r.alamatRumah);
+    }
+}
