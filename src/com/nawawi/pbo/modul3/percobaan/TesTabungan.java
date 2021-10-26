dc3f9e1092dde9ae485fc3985a65798597a8ffb8
AhmadMCer <ahmadmcer@gmail.com>
2021-10-26 10:16:42 +0700

    fix: add spaces

--- src/com/nawawi/pbo/modul3/percobaan/TesTabungan.java
+++ src/com/nawawi/pbo/modul3/percobaan/TesTabungan.java
@@ -4,8 +4,10 @@ public class TesTabungan {
     public static void main(String[] args) {
         Tabungan t = new Tabungan(5000);
         System.out.println("Saldo awal tabungan anda: " + t.saldo);
+
         t.ambilUang(1500);
         System.out.println("Jumlah uang yang diambil: 1500");
+
         System.out.println("Saldo tabungan anda sekarang adalah: " + t.saldo);
     }
 }
