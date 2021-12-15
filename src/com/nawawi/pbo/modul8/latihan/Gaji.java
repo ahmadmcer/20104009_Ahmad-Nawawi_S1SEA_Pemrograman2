1ec8baa53b1dbfc268e11eeba32b21439aeddae7
AhmadMCer <ahmadmcer@gmail.com>
2021-12-15 21:17:50 +0700

    feat: add modul 8 latihan

---
+++ src/com/nawawi/pbo/modul8/latihan/Gaji.java
@@ -0,0 +1,30 @@
+package com.nawawi.pbo.modul8.latihan;
+
+public class Gaji extends Pegawai{
+    private double salary;
+
+    public Gaji(String name, String address, int number, double salary) {
+        super(name, address, number);
+        setSalary(salary);
+    }
+
+    public void mailCheck() {
+        System.out.println("Memerika kelas gaji dalam surat");
+        System.out.println("Surat tertuju untuk " + getName() + " dengan gaji " + salary);
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double newSalary) {
+        if (newSalary >= 0.0) {
+            salary = newSalary;
+        }
+    }
+
+    public double computePay() {
+        System.out.println("Menghitung pembayaran gaji untuk " + getName());
+        return salary / 52;
+    }
+}
---
+++ src/com/nawawi/pbo/modul8/latihan/VirtualDemo.java
@@ -0,0 +1,14 @@
+package com.nawawi.pbo.modul8.latihan;
+
+public class VirtualDemo {
+    public static void main(String[] args) {
+        Gaji s = new Gaji("Wahyu", "KUBAR", 3, 5000.00);
+        Pegawai e = new Gaji("Ini nama", "Samarinda", 2, 2500.00);
+
+        System.out.println("Memanggil mailCheck berdasarkan referensi Gaji --");
+        s.mailCheck();
+
+        System.out.println("\nMemanggil mailCheck berdasarkan referensi Pegawai --");
+        e.mailCheck();
+    }
+}
